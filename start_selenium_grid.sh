406309d550ffa269a5e7853a69428c43e867a537
Raymond von Mizener <vmizener@gmail.com>
2018-10-24 15:10:52 -0700

    Now starts arbitrarily large number of nodes

--- start_selenium_grid.sh
+++ start_selenium_grid.sh
@@ -6,12 +6,22 @@ if [[ ! -f ${SELENIUM_GRID_JAR_LOCATION}/selenium-server-standalone-${SELENIUM_S
     >&2 echo "Try running the get_selenium_grid.sh script to download this version"
     exit 1
 else
-    echo "Starting server versioned: ${SELENIUM_SERVER_VERSION}"
-    java -jar ${SELENIUM_GRID_JAR_LOCATION}/selenium-server-standalone-${SELENIUM_SERVER_VERSION}.jar -role hub -port 4441 &> ${GRID_HUB_LOGFILE} &
+    if (( ${GRID_NODE_COUNT} > ${#GRID_NODE_PORT_OPTIONS[@]} )); then
+        >&2 echo "Not enough grid node port options defined in config (got ${#GRID_NODE_PORT_OPTIONS[@]}, need at least ${GRID_NODE_COUNT})"
+        exit 1
+    fi
+    java -jar ${SELENIUM_GRID_JAR_LOCATION}/selenium-server-standalone-${SELENIUM_SERVER_VERSION}.jar -role hub -port ${GRID_HUB_PORT} &> ${GRID_HUB_LOGFILE} &
     sleep 3
+    >&2 echo "Started server versioned: ${SELENIUM_SERVER_VERSION}"
     register_address=$(head ${GRID_HUB_LOGFILE} | grep -o 'http://[0-9a-z.:/]\+register/')
-    java -jar ${SELENIUM_GRID_JAR_LOCATION}/selenium-server-standalone-${SELENIUM_SERVER_VERSION}.jar -role node -hub $register_address -port 3456 &> ${GRID_NODE_LOGFILE1} &
-    java -jar ${SELENIUM_GRID_JAR_LOCATION}/selenium-server-standalone-${SELENIUM_SERVER_VERSION}.jar -role node -hub $register_address -port 4567 &> ${GRID_NODE_LOGFILE2} &
+    count=0
+    while (( ${count} < ${GRID_NODE_COUNT} )); do
+        grid_node_port=${GRID_NODE_PORT_OPTIONS[${count}]}
+        count=$((count+1))
+        grid_node_logfile=$(printf ${GRID_NODE_LOGFILE_FORMAT} ${count})
+        java -jar ${SELENIUM_GRID_JAR_LOCATION}/selenium-server-standalone-${SELENIUM_SERVER_VERSION}.jar -role node -hub ${register_address} -port ${grid_node_port} &> ${grid_node_logfile} &
+        >&2 echo "Attached node ${count} to port ${grid_node_port} with logfile ${grid_node_logfile}"
+    done
     hub_address=$(head ${GRID_HUB_LOGFILE} | grep -o 'http://[0-9a-z.:/]\+hub')
-    echo $hub_address | tee ${HUB_ADDR_OUTFILE}
+    echo ${hub_address} | tee ${HUB_ADDR_OUTFILE}
 fi
