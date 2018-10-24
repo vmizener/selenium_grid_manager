f7156bbb3ea9f0589ed7d8d31d587de296a9e70c
Raymond von Mizener <vmizener@gmail.com>
2018-10-24 14:26:25 -0700

    Cleaned up internals

--- kill_selenium_grid.sh
+++ kill_selenium_grid.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 source config.env
 
-case `uname -a` in
+case $(uname -a) in
     *"Linux"*)
-        MATCHSET=`ps a | grep ".*[s]elenium-server-standalone-${SELENIUM_SERVER_VERSION}.jar.*"`
+        MATCHSET=$(ps a | grep ".*[s]elenium-server-standalone-${SELENIUM_SERVER_VERSION}.jar.*")
         ;;
     *"Cygwin"*)
-        MATCHSET=`ps -a | grep -o '[0-9].*/java$'`
+        MATCHSET=$(ps -a | grep -o '[0-9].*/java$')
         ;;
     *"Darwin"*)
-        MATCHSET=`ps -e | grep -o "\d\+.*[s]elenium-server-standalone-${SELENIUM_SERVER_VERSION}.jar.*"`
+        MATCHSET=$(ps -e | grep -o "\d\+.*[s]elenium-server-standalone-${SELENIUM_SERVER_VERSION}.jar.*")
         ;;
 esac
 
--- start_selenium_grid.sh
+++ start_selenium_grid.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 source config.env
 
-if [ ! -f ${SELENIUM_GRID_JAR_LOCATION}/selenium-server-standalone-${SELENIUM_SERVER_VERSION}.jar ]; then
-    echo "Couldn't find server versioned: ${SELENIUM_SERVER_VERSION}"
+if [[ ! -f ${SELENIUM_GRID_JAR_LOCATION}/selenium-server-standalone-${SELENIUM_SERVER_VERSION}.jar ]]; then
+    >&2 echo "Couldn't find server versioned: ${SELENIUM_SERVER_VERSION}"
+    >&2 echo "Try running the get_selenium_grid.sh script to download this version"
     exit 1
 else
     echo "Starting server versioned: ${SELENIUM_SERVER_VERSION}"
     java -jar ${SELENIUM_GRID_JAR_LOCATION}/selenium-server-standalone-${SELENIUM_SERVER_VERSION}.jar -role hub -port 4441 &> ${GRID_HUB_LOGFILE} &
     sleep 3
-    register_address=`head ${GRID_HUB_LOGFILE} | grep -o 'http://[0-9a-z.:/]\+register/'`
+    register_address=$(head ${GRID_HUB_LOGFILE} | grep -o 'http://[0-9a-z.:/]\+register/')
     java -jar ${SELENIUM_GRID_JAR_LOCATION}/selenium-server-standalone-${SELENIUM_SERVER_VERSION}.jar -role node -hub $register_address -port 3456 &> ${GRID_NODE_LOGFILE1} &
     java -jar ${SELENIUM_GRID_JAR_LOCATION}/selenium-server-standalone-${SELENIUM_SERVER_VERSION}.jar -role node -hub $register_address -port 4567 &> ${GRID_NODE_LOGFILE2} &
-    hub_address=`head ${GRID_HUB_LOGFILE} | grep -o 'http://[0-9a-z.:/]\+hub'`
-    echo $hub_address > ${HUB_ADDR_OUTFILE}
-    echo $hub_address
+    hub_address=$(head ${GRID_HUB_LOGFILE} | grep -o 'http://[0-9a-z.:/]\+hub')
+    echo $hub_address | tee ${HUB_ADDR_OUTFILE}
 fi
